e1f4c7e429dba463a918195bbbe5ddac6e01d250
Johan Engstrand <johan@engstrand.nu>
2020-11-13 21:34:24 +0100

    feat: fetch articles in news block directly, no cron

--- blocks.h
+++ blocks.h
@@ -23,7 +23,7 @@
 static Block blocks[] = {
   /* funcu                 funcc                   interval        signal */
   {  musicu,                musicc,                0,              9       },
-  {  newsu,                newsc,                  5,              6       },
+  {  newsu,                newsc,                  45,             6       },
   /* {  cputempu,             cputempc,               2,              6       }, */
   /* {  mailu,                mailc,                  300,            5       }, */
   {  kblayoutu,            kblayoutc,              0,              7       },
--- blocks/news.c
+++ blocks/news.c
@@ -5,16 +5,22 @@
 
 #define ICON0               ""
 
-#define UNREADPATH          "/tmp/newsupdate"
+#define NEWSBOATRELOAD      (char *[]){ "/usr/bin/newsboat", \
+                                        "--cache-file=/tmp/newsboatreload", \
+                                        "-x", "reload", \
+                                        "-x", "print-unread", \
+                                        NULL }
+#define BUFLENGTH           5
 
 void
 newsu(char *str, int sigval, BlockData *blockdata)
 {
-  int unread;
-  if (readint(UNREADPATH, &unread)) {
-    snprintf(str, CMDLENGTH, BLOCK_SUCCESS(ICON(ICON0), "%d"), unread);
-  }
-  else {
+  char buf[BUFLENGTH];
+  buf[getcmdout(NEWSBOATRELOAD, buf, BUFLENGTH) - 1] = '\0';
+  char *unread = strtok(buf, " ");
+  if (strcmp(unread, "0")) {
+    snprintf(str, CMDLENGTH, BLOCK_SUCCESS(ICON(ICON0), "%s"), unread);
+  } else {
     printempty(str);
   }
 }
@@ -22,12 +28,12 @@ newsu(char *str, int sigval, BlockData *blockdata)
 void
 newsc(int button, BlockData *blockdata)
 {
-  switch (button){
+  switch (button) {
     case 1:
       TERMCMD("newsboat");
       break;
     case 2:
-      TERMCMD("newsup");
+      csigself(6,1);
       break;
   }
 }
